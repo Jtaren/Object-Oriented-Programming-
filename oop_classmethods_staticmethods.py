694c6fc861fe15b7b62a14b9d2442ec89c27e783
Solomon Jt Aren <78317210+Jtaren@users.noreply.github.com>
2024-03-04 19:55:14 +0100

    Update oop_classmethods_staticmethods.py

--- oop_classmethods_staticmethods.py
+++ oop_classmethods_staticmethods.py
@@ -1,7 +1,7 @@
 class Employee:
 
   num_of_emps = 0
-  raise_amount = 1.04
+  raise_amt = 1.04
   
   def __init__(self, first, last, pay):
     self.first = first
@@ -17,9 +17,14 @@ class Employee:
   def apply_raise(self):
     self.pay = int(self.pay * Employee.raise_amount)
 
+  @classmethod
+  def set_raise_amt(cls, amount):
+    cls.raise_amt = amount
+
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'User', 60000)
 
+"""
 print(Employee.num_of_emps)
 
 print(emp_1.pay)
@@ -29,7 +34,8 @@ print(emp_1.pay)
 print(Employee.__dict__)
 print(emp_1.__dict__)
 emp_1.raise_amount = 1.05
+"""
 
-print(Employee.raise_amount)
-print(emp_1.raise_amount)
-print(emp_2.raise_amount)
+print(Employee.raise_amt)
+print(emp_1.raise_amt)
+print(emp_2.raise_amt)
