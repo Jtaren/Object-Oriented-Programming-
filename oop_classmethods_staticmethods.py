8e1d0eb23a827a75fc742909e47513ff8a5594f0
Solomon Jt Aren <78317210+Jtaren@users.noreply.github.com>
2024-03-04 20:33:43 +0100

    Update oop_classmethods_staticmethods.py

--- oop_classmethods_staticmethods.py
+++ oop_classmethods_staticmethods.py
@@ -21,16 +21,24 @@ class Employee:
   def set_raise_amt(cls, amount):
     cls.raise_amt = amount
 
+  @classmethod
+  def from_string(cls, emp_str):
+    first, last, pay = emp_str.split('-')
+    return cls(first, last, pay)
+
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'User', 60000)
 
 emp_str_1 = 'John-Doe-70000'
 emp_str_2 = 'Steve-Smith-30000'
 emp_str_3 = 'Jane-Doe-90000'
-
+"""
 first, last, pay = emp_str_1.split('-')
-
+"""
+"""
 new_emp_1 = Employee(first, last, pay)
+"""
+new_emp_1 = Employee.from_string(emp_str_1)
 
 print(new_emp_1.email)
 print(new_emp_1.pay)
