4d35ea34e3e5726c336927d005c087ce494a7fde
jtaren <arenjatau@gmail.com>
2024-03-06 04:03:22 +0100

    oop_special_methods.py

--- oop_special_methods.py
+++ oop_special_methods.py
@@ -8,18 +8,25 @@ class Employee:
     self.email = first + '.' + last + '@email.com'
     self.pay = pay
 
-def fullname(self):
-  return '{} {}'.format(self.first, self.last)
+  def fullname(self):
+    return '{} {}'.format(self.first, self.last)
 
-def apply_raise(self):
-  self.pay = int(self.pay * self.raise_amt)
+  def apply_raise(self):
+    self.pay = int(self.pay * self.raise_amt)
 
-def __repr__(self):
-  pass
+  def __repr__(self):
+    return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
 
-def __str__(self):
-  pass
+  def __str__(self):
+    return '{} - {}'.format(self.fullname(), self.email)
 
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'Employee', 60000)
-    
+
+print(emp_1) 
+
+print(repr(emp_1))
+print(str(emp_1))
+
+print(emp_1.__repr__())
+print(emp_1.__str__())
