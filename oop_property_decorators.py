d36bb9b40ab0fbe0bd71cbe13a54305d8c15043c
jtaren <arenjatau@gmail.com>
2024-03-07 20:09:23 +0100

    oop_property_decorators.py

--- oop_property_decorators.py
+++ oop_property_decorators.py
@@ -22,16 +22,26 @@ class Employee:
     self.first = first
     self.last = last
 
+  @property
   def email(self):
     return '{}.{}@email.com'.format(self.first, self.last)
 
+  @property
   def fullname(self):
     return '{} {}'.format(self.first, self.last)
+  
+  @fullname.setter
+  def fullname(self, name):
+    first, last = name.split(' ')
+    self.first = first
+    self.last = last
 
 emp_1 = Employee('John', 'Smith')
 
 emp_1.first = 'Jim'
 
+emp_1.fullname = 'Corey Schafer'
+
 print(emp_1.first)
-print(emp_1.email())
-print(emp_1.fullname())
+print(emp_1.email)
+print(emp_1.fullname)
